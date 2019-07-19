fba15e98414965ede9f1b54584b5c28bfc8114dd
kindboy <2515418556@qq.com>
2019-07-19 23:46:49 +0800

    feat: 文件管理页添加分页功能

--- src/pages/Files/index.tsx
+++ src/pages/Files/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { PageHeader, Upload, Icon, message } from 'antd';
+import { PageHeader, Upload, Icon, Pagination, Spin, message } from 'antd';
 import { UploadChangeParam, UploadFile } from 'antd/lib/upload/interface';
 
 import BaseComponent from '@/pages/components/BaseComponent';
@@ -8,6 +8,7 @@ import PageLoading from '@/components/PageLoading';
 import { AppState } from '@/store';
 import { getFileList } from '@/store/files/thunks';
 import { Props, State } from './interface';
+import './index.scss';
 
 const { Dragger } = Upload;
 
@@ -18,11 +19,18 @@ class Files extends BaseComponent<Props, State> {
     }
 
     async componentDidMount() {
-        const res = await this.props.getFileList({
-            pageNo: this.props.pagination.pageNo
+        await this.getFileListData();
+        this.setState({
+            inited: true
+        });
+    }
+
+    getFileListData = async (pageNo = 1, pageSize = 40) => {
+        await this.props.getFileList({
+            pageNo,
+            pageSize
         });
         this.setState({
-            inited: true,
             showFileList: this.props.fileList.map((file: MFile) => ({
                 uid: file.id,
                 size: file.size,
@@ -47,18 +55,7 @@ class Files extends BaseComponent<Props, State> {
         if (status === 'done') {
             if (info.file.response.code === 0) {
                 message.success(`${info.file.name} file uploaded successfully.`);
-                await this.props.getFileList({
-                    pageNo: 1
-                });
-                this.setState({
-                    showFileList: this.props.fileList.map((file: MFile) => ({
-                        uid: file.id,
-                        size: file.size,
-                        name: file.originName,
-                        url: file.path,
-                        type: 'img'
-                    }))
-                });
+                await this.getFileListData();
             } else {
                 message.error(`${info.file.name} file upload failed.`);
             }
@@ -67,27 +64,52 @@ class Files extends BaseComponent<Props, State> {
         }
     }
 
+    handleRemove = (file: UploadFile) => {
+        console.log(file);
+    }
+
+    handlePaginationChange = (page: number, pageSize: number) => {
+        this.getFileListData(page, pageSize);
+    }
+
+    handlePaginationSizeChange = (page: number, pageSize: number) => {
+        this.getFileListData(page, pageSize);
+    }
+
     render() {
+        const { pageNo, pageSize, total } = this.props.pagination;
         return this.state.inited ? (
             <div className="page c-page-files">
                 <PageHeader title="文件管理" />
                 <div className="page-content">
-                    <Dragger
-                        multiple
-                        name="file"
-                        action="/api/upload"
-                        headers={{
-                            Authorization: `TOKEN ${JSON.parse(localStorage.getItem('TOKEN')).token}`
-                        }}
-                        listType="picture-card"
-                        fileList={this.state.showFileList}
-                        onChange={this.handleChange}
-                    >
-                        <p className="ant-upload-drag-icon">
-                            <Icon type="inbox" />
-                        </p>
-                        点击或者拖动文件至此上传
-                    </Dragger>
+                    <Spin spinning={this.props.isLoadingListData}>
+                        <Dragger
+                            multiple
+                            name="file"
+                            action="/api/upload"
+                            headers={{
+                                Authorization: `TOKEN ${JSON.parse(localStorage.getItem('TOKEN')).token}`
+                            }}
+                            listType="picture-card"
+                            fileList={this.state.showFileList}
+                            onChange={this.handleChange}
+                            onRemove={this.handleRemove}
+                        >
+                            <p className="ant-upload-drag-icon">
+                                <Icon type="inbox" />
+                            </p>
+                            点击或者拖动文件至此上传
+                        </Dragger>
+                    </Spin>
+                    <Pagination
+                        showQuickJumper
+                        showSizeChanger
+                        defaultCurrent={pageNo}
+                        defaultPageSize={pageSize}
+                        total={total}
+                        onChange={this.handlePaginationChange}
+                        onShowSizeChange={this.handlePaginationSizeChange}
+                    />
                 </div>
             </div>
         ) : <PageLoading />;
